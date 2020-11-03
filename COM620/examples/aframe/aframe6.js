8b034e5b736da24260bf80b451778b2191adad56
Nick Whitelegg <nickw4426@gmail.com>
2020-11-03 18:55:54 +0200

    really add geoloc

--- COM620/examples/aframe/aframe6.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent("tester", {
-	init: function() {
-		this.el.addEventListener('click', e=> {
-			alert('clicked');
-			this.el.setAttribute('material', { color: 'blue'});
-			this.el.setAttribute('geometry', { primitive: 'box'});
-		});
-	}
-});
