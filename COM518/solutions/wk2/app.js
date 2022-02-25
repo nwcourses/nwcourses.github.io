fc2d0490d4585264e019d83ad1a5992335e590ff
Nick Whitelegg <nickw4426@gmail.com>
2022-02-25 13:27:40 +0000

    COM518 update to reference solutions

--- COM518/solutions/wk2/app.js
+++ COM518/solutions/wk2/app.js
@@ -1,10 +1,16 @@
 // Solution to the standard exercises for week 2
 
+// Require dependencies
 const express = require('express');
-const app = express();
-app.use(express.json());
 const mysql = require('mysql2');
 
+// Create our Express server.
+const app = express();
+
+// Enable reading JSON from the request body of POST requests
+app.use(express.json());
+
+// Create a database connection to the server - please edit to use your details
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'ephp001',
@@ -12,10 +18,16 @@ const con = mysql.createConnection({
     database: 'ephp001'}
 );
 
+// Try to connect to the server
 con.connect(err => {
+    // If it fails, display an error
     if(err) {
         console.log(`Cannot connect to database: ${err}`);
+        process.exit(1);
     } else {
+        // If successful, set up the routes
+
+        // Search by artist
         app.get('/artist/:artist', (req, res) => {
             con.query('SELECT * FROM wadsongs WHERE artist=?',
                 [req.params.artist],
@@ -28,6 +40,7 @@ con.connect(err => {
                 });
         });
 
+        // Search by title
         app.get('/title/:title', (req, res) => {
             con.query('SELECT * FROM wadsongs WHERE title=?',
                 [req.params.title],
@@ -40,9 +53,10 @@ con.connect(err => {
                 });
         });
 
+        // Search by title and artist
         app.get('/artist/:artist/title/:title', (req, res) => {
             con.query('SELECT * FROM wadsongs WHERE title=? AND artist=?',
-                [req.params.title, req.params.artist],
+                [req.params.title, req.params.artist], // note two bound parameters, so we create an array with two members
                 (err, results, fields) => {
                     if(err) {
                         res.status(500).json({error: err});
@@ -52,12 +66,13 @@ con.connect(err => {
                 });
         });
 
+        // Buy a song with a given ID
         app.post('/song/:id/buy', (req, res) => {
             con.query('UPDATE wadsongs SET quantity=quantity-1 WHERE ID=?',
                 [req.params.id],
                 (err, results, fields) => {
                     if(err) {
-                        res.status(500).json({errror: err});
+                        res.status(500).json({error: err});
                     } else if (results.affectedRows == 0) {
                         res.status(404).json({error: 'No song with that ID!'});
                     } else {
@@ -66,23 +81,26 @@ con.connect(err => {
                 });
         });
 
+        // Create a new song, using a JSON object passed in the request body
         app.post('/newsong', (req, res) => {
             con.query('INSERT INTO wadsongs(title,artist,day,month,year,chart,review) VALUES(?,?,?,?,?,?,?)',
                 [req.body.title, req.body.artist, req.body.day, req.body.month, req.body.year, req.body.chart, req.body.review],
                 (err, results, fields) => {
                     if(err) {
-                        res.status(500).json({errror: err});
+                        res.status(500).json({error: err});
                     } else {
                         res.json(results);
                     }
                 });
         }); 
+
+        // Delete a song
         app.delete('/song/:id', (req, res) => {
             con.query('DELETE FROM wadsongs WHERE ID=?',
                 [req.params.id],
                 (err, results, fields) => {
                     if(err) {
-                        res.status(500).json({errror: err});
+                        res.status(500).json({error: err});
                     } else if (results.affectedRows == 0) {
                         res.status(404).json({error: 'No song with that ID!'});
                     } else {
@@ -91,6 +109,7 @@ con.connect(err => {
                 });
         });
 
+        // Run the server on port 3000
         app.listen(3000);
     }
 });
