4964864d9c3f7de3f05f8afd26831fe6a0c8da01
Nick Whitelegg <nickw4426@gmail.com>
2023-07-11 12:58:44 +0100

    Breadth first search clarification

--- COM421/examples/recursion.py
+++ COM421/examples/recursion.py
@@ -3,10 +3,10 @@ import traceback
 def recursive_print(num, limit):
 	print(f"recursive_print(): parameters: num {num} limit {limit}")
 #	traceback.print_stack()
+	print(num)
 
-	if num <= limit:
+	if num < limit:
 		print(f"num does not exceed {limit}, so calling recursive_print again with argument of {num+1}")
-		print(num)
 		recursive_print(num+1, limit)
 
 	print(f"Returning from recursive_print call with a parameter of {num}")
